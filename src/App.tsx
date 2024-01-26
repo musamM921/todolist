66e58485c4ee2f7a025677e6d82320801e94bf4b
musamM921 <musaartlove@gmail.com>
2024-01-26 16:31:39 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import TodoList from "./TodoList"; // импортируйте как дефолтный экспорт
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
